ed72c82e16e8f11a06c08da983999031b8947986
Ismail mayito <ismayito1@gmail.com>
2023-09-01 21:50:01 +0300

    Final touches to make the app close to wireframe

--- src/modules/refresh.js
+++ src/modules/refresh.js
@@ -2,18 +2,25 @@ import displayListUI from './displayListUI.js';
 
 const refresh = async () => {
   const id = localStorage.getItem('gameId');
-  const refreshBtn = document.getElementById('btn-refresh');
   const list = document.getElementById('list-id');
+  const listContainer = document.getElementById('list-container');
 
   const data = await fetch(
     `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores`,
   );
 
   const response = await data.json();
-  refreshBtn.addEventListener('click', () => {
-    list.innerHTML = '';
+
+  // showing the list container dynamically
+  if (response.result.length === 0) {
+    listContainer.style.display = 'none';
+  } else {
+    listContainer.style.display = 'block';
+  }
+  list.innerHTML = '';
+  if (id) {
     displayListUI(response.result);
-  });
+  }
   return response;
 };
 
